61d3eacde5d1d2f736b7c6fa06a8d8d75f36f5e0
EliasMart1ns <eliasfogacamartinsjr@gmail.com>
2026-05-18 08:49:44 -0300

    3 atividades

--- script.js
+++ script.js
@@ -23,3 +23,38 @@ function parOuimpar(a, b){
   return "impar";
 }
 module.exports = { parOuimpar };
+
+function votar(idade){
+  if(idade<16){
+    return "não pode votar"
+  }
+  if(idade<18 || idade>=70){
+    return "Voto opcional"
+  }
+  return "Pode votar"
+}
+module.exports={ votar };
+
+function numero(valor){
+
+  if(valor>0){
+    return "positivo";
+  }
+  if(valor<0){
+    return "Negativo";
+  }
+  return "Zero";
+}
+module.exports={ numero };
+
+function bonus(salario){
+
+  if(salario<2000){
+    return salario*0.20;
+  }
+  if(salario<=5000){
+    return salario*0.10;
+  }
+  return salario*0.05;
+}
+module.exports={ bonus };
--- script.test.js
+++ script.test.js
@@ -18,4 +18,22 @@ const { parOuimpar } = require('./script');
 
 test('Par ou impar', () =>{
   expect(parOuimpar(4)).toBe("Par");
-})
+});
+
+const { votar } = require('./script');
+
+test('Votar', ()=>{
+  expect(votar(15)).toBe("Não pode votar");
+});
+
+const { numero } = requise('./script');
+
+test('numero', ()=>{
+  expect(numero(5)).toBe("positivo");
+});
+
+const { bonus } = requise('./script');
+
+test('bonus', ()=>{
+  expect(bonus(1000)).toBe(200);
+});
